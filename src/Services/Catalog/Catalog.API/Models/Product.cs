0c607f59f75c5e9a60a4b9cff0ecf7393d76f74c
durpintm <durpinthapa@gmail.com>
2024-12-23 20:45:19 -0500

    Seeding data to Products table

--- src/Services/Catalog/Catalog.API/Models/Product.cs
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -7,5 +7,5 @@ public class Product
     public List<string> Category { get; set; } = new();
     public string Description { get; set; } = default!;
     public string ImageFile { get; set; } = default!;
-    public int Price { get; set; }
+    public decimal Price { get; set; }
 }
