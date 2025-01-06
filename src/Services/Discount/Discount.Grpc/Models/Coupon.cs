4930e975b5703b82318f93f7ab19a0a3af9b9cb8
durpintm <durpinthapa@gmail.com>
2025-01-05 19:54:43 -0500

    Changing amount data type

--- src/Services/Discount/Discount.Grpc/Models/Coupon.cs
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -4,6 +4,6 @@ public class Coupon
     public int Id { get; set; }
     public string ProductName { get; set; } = default!;
     public string Description { get; set; } = default!;
-    public decimal Amount { get; set; }
+    public double Amount { get; set; }
 }
 
