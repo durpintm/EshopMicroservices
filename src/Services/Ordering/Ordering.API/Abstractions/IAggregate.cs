fc6102859ae3c3cbd65e1055f5b980193c3bcdb9
durpinlc <durpinthapamagar@loyalistcollege.com>
2025-02-03 19:39:11 -0500

    Aggregates added

---
+++ src/Services/Ordering/Ordering.API/Abstractions/IAggregate.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.API.Abstractions;
+
+public interface IAggregate<T> : IAggregate, IEntity<T>
+{
+
+}
+
+public interface IAggregate : IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+
+    IDomainEvent[] ClearDomainEvents();
+}
+
