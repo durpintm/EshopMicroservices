c05ad309fbe4b865fdfcf43eb561bfefb27e581f
durpintm <durpinthapa@gmail.com>
2025-02-10 22:02:39 -0500

    Added methods to create, update, add and remove order items

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -1,7 +1,7 @@
 ﻿namespace Ordering.Domain.Models;
 public class OrderItem : Entity<OrderItemId>
 {
-    public OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
+    internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
     {
         Id = OrderItemId.Of(Guid.NewGuid());
         OrderId = orderId;
