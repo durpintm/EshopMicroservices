8eff1d2260c2b529b99bb74daa705408c2c6cee3
durpintm <durpinthapa@gmail.com>
2025-02-14 22:59:19 -0500

    Added global usings in ordering infrastructure

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Ordering.Infrastructure
+namespace Ordering.Infrastructure;
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
-        {
-            var connectionString = configuration.GetConnectionString("Database");
-            return services;
-        }
+        var connectionString = configuration.GetConnectionString("Database");
+
+        // Add services to the container.
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+        return services;
     }
 }
