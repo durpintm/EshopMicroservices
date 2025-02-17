1a18662f6de9a41be5540b08eaa55da216c449b9
durpintm <durpinthapa@gmail.com>
2025-02-16 22:25:35 -0500

    Seeding data in Product and Order entities

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -5,14 +5,14 @@ public record Payment
     public string CardNumber { get; } = default!;
     public string Expiration { get; } = default!;
     public string CVV { get; } = default!;
-    public string PaymentMethod { get; } = default!;
+    public int PaymentMethod { get; } = default!;
 
     protected Payment()
     {
 
     }
 
-    private Payment(string cardName, string cardNumber, string expiration, string cvv, string paymentMethod)
+    private Payment(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
     {
         CardName = cardName;
         CardNumber = cardNumber;
@@ -21,7 +21,7 @@ public record Payment
         PaymentMethod = paymentMethod;
     }
 
-    public static Payment Of(string cardName, string cardNumber, string expiration, string cvv, string paymentMethod)
+    public static Payment Of(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
         ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -19,8 +19,8 @@ public static class DatabaseExtensions
     private static async Task SeedAsync(ApplicationDbContext context)
     {
         await SeedCustomerAsync(context);
-        //await SeedProductAsync(context);
-        //await SeedOrderAndItemsAsync(context);
+        await SeedProductAsync(context);
+        await SeedOrderWithItemsAsync(context);
     }
 
     private static async Task SeedCustomerAsync(ApplicationDbContext context)
@@ -31,5 +31,22 @@ public static class DatabaseExtensions
             await context.SaveChangesAsync();
         }
     }
-}
 
+    private static async Task SeedProductAsync(ApplicationDbContext context)
+    {
+        if (!await context.Products.AnyAsync())
+        {
+            await context.Products.AddRangeAsync(InitialData.Products);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedOrderWithItemsAsync(ApplicationDbContext context)
+    {
+        if (!await context.Orders.AnyAsync())
+        {
+            await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+            await context.SaveChangesAsync(); 
+        }
+    }
+}
