0dea508275977d2cc1c34627b3bdf8e350736868
durpintm <durpinthapa@gmail.com>
2025-02-20 21:52:44 -0500

    Adding interceptor for dispatching domain events

--- src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
@@ -8,7 +8,6 @@ public interface IAggregate<T> : IAggregate, IEntity<T>
 public interface IAggregate : IEntity
 {
     IReadOnlyList<IDomainEvent> DomainEvents { get; }
-
     IDomainEvent[] ClearDomainEvents();
 }
 
