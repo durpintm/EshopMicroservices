74792a20258dfdeff28ecc6b6b873850b35cf8ea
durpintm <durpinthapa@gmail.com>
2025-03-04 21:02:14 -0500

    Added extension method for ToOrderDto

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,10 +1,16 @@
-﻿namespace Ordering.Application.Orders.EventHandlers.Domain;
-public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+﻿using MassTransit;
+using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+public class OrderCreatedEventHandler(IPublishEndpoint publishEndpoint, ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
 {
-    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType());
-        return Task.CompletedTask;
+        logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+        var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+
+        await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
     }
 }
 
